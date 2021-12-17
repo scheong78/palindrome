f35415a0002e901723f7bcd50b0c3d3a1a7ea7a5
Stan the Man <noemail>
2021-12-17 10:22:25 -0500

    Initialize repository

---
+++ index.js
@@ -0,0 +1,32 @@
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
+}
+
+// Defines a Phrase object.
+function Phrase(content) {
+  this.content = content;
+
+  // Returns lowercased content
+  this.processedContent = function() {
+    return this.content.toLowerCase();
+  }
+
+  // Return true if it is a palindrome. Return false otherwise
+  this.palindrome = function() {
+    return this.processedContent() === this.processedContent().reverse();
+  }
+}
+
+// // Defines a TranslatedPhrase object.
+// function TranslatedPhrase(content, translation) {
+//   this.content = content;
+//   this.translation = translation;
+//
+//   // Returns translation processed for palindrome testing.
+//   this.processedContent = function() {
+//     return this.translation.toLowerCase();
+//   }
+// }
+//
+// TranslatedPhrase.prototype = new Phrase();
